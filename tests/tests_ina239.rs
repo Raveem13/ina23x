8ba2a2c10fda9f7c3ab4f20c6e674d0308dccc56
Raveem KG <gkraveem@gmail.com>
2026-08-20 17:49:13 +0530

    test(ina239-spi): Fix INA239 mock tests with concatenated transactions
    - Wrap spitransaction with transactions start() & end()
    - Use `.concat()` to flatten nested `SpiTransaction` arrays cleanly.
    - Cover raw register reading, writing, and bitwise masking logic.

--- tests/tests_ina239.rs
+++ tests/tests_ina239.rs
@@ -25,13 +25,10 @@ fn default() {
 // Reset test
 #[test]
 fn reset_test() {
-    let expectations = [
-        write_txn(0x00, 1 << 15), 
-        read_txn(0x05, &[0x00, 0x00]),
-    ];
-    let mut spi = SpiMock::new(&expectations);
+    let expected_txns = [write_txn(0x00, 1 << 15), read_txn(0x05, &[0x00, 0x00])].concat();
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
-    // ina.reset().unwrap();
+    ina.reset().unwrap();
     //Check busvoltage after reset
     assert_eq!(ina.bus_voltage().unwrap(), 0.0);
     spi.done();
@@ -40,7 +37,7 @@ fn reset_test() {
 // Test to get manufacture ID
 #[test]
 fn test_manufacture_id() {
-    let mut spi = SpiMock::new(&[read_txn(0x3E, &[0x54, 0x49])]);
+    let mut spi = SpiMock::new(&[read_txn(0x3E, &[0x54, 0x49])].concat());
     let mut ina = INA239::new(spi.clone());
     let id = ina.manufacture_id().unwrap();
     assert_eq!(id, 0x5449);
@@ -50,10 +47,10 @@ fn test_manufacture_id() {
 // Test to get device ID
 #[test]
 fn test_device_id() {
-    let mut spi = SpiMock::new(&[read_txn(0x3F, &[0x23, 0x91])]);
+    let mut spi = SpiMock::new(&[read_txn(0x3F, &[0x23, 0x91])].concat());
     let mut ina = INA239::new(spi.clone());
     let did = ina.device_id().unwrap();
-    assert_eq!(did, (0x238, 0x1));
+    assert_eq!(did, (0x239, 0x1));
     spi.done();
 }
 
@@ -61,7 +58,7 @@ fn test_device_id() {
 #[test]
 fn test_calibration() {
     let shunt_cal = exp_shunt_cal(20.0, 0.1, false);
-    let mut spi = SpiMock::new(&[write_txn(0x02, shunt_cal)]);
+    let mut spi = SpiMock::new(&[write_txn(0x02, shunt_cal)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_shunt_calibrate(20.0, 0.1).unwrap();
     spi.done();
@@ -71,7 +68,7 @@ fn test_calibration() {
 #[test]
 fn test_configuration() {
     let confg_val = exp_confg(10, true);
-    let mut spi = SpiMock::new(&[write_txn(0x00, confg_val)]);
+    let mut spi = SpiMock::new(&[write_txn(0x00, confg_val)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_config(10, Range40mV).unwrap();
     spi.done();
@@ -81,7 +78,7 @@ fn test_configuration() {
 #[test]
 fn test_adc_configuration() {
     let exp_val = exp_adc_cfg(ContinuousAll, Ct1052, Ct1052, Ct1052, AvgN1024);
-    let mut spi = SpiMock::new(&[write_txn(0x01, exp_val)]);
+    let mut spi = SpiMock::new(&[write_txn(0x01, exp_val)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_adc_config(ContinuousAll, Ct1052, Ct1052, Ct1052, AvgN1024)
         .unwrap();
@@ -91,7 +88,7 @@ fn test_adc_configuration() {
 // Shunt voltage test
 #[test]
 fn test_shunt_volt() {
-    let mut spi = SpiMock::new(&[read_txn(0x04, &0x0020_u16.to_be_bytes())]);
+    let mut spi = SpiMock::new(&[read_txn(0x04, &0x0020_u16.to_be_bytes())].concat());
     let mut ina = INA239::new(spi.clone());
     ina.shunt_voltage().unwrap();
     spi.done();
@@ -100,7 +97,7 @@ fn test_shunt_volt() {
 // Bus voltage test
 #[test]
 fn test_bus_volt() {
-    let mut spi = SpiMock::new(&[read_txn(0x05, &0x0010_u16.to_be_bytes())]);
+    let mut spi = SpiMock::new(&[read_txn(0x05, &0x0010_u16.to_be_bytes())].concat());
     let mut ina = INA239::new(spi.clone());
     ina.bus_voltage().unwrap();
     spi.done();
@@ -109,7 +106,7 @@ fn test_bus_volt() {
 // Internal die temperature test
 #[test]
 fn test_die_temp() {
-    let mut spi = SpiMock::new(&[read_txn(0x06, &0x0040_u16.to_be_bytes())]);
+    let mut spi = SpiMock::new(&[read_txn(0x06, &0x0040_u16.to_be_bytes())].concat());
     let mut ina = INA239::new(spi.clone());
     ina.die_temperature().unwrap();
     spi.done();
@@ -118,7 +115,7 @@ fn test_die_temp() {
 // Current result test
 #[test]
 fn test_current() {
-    let mut spi = SpiMock::new(&[read_txn(0x07, &0x0001_u16.to_be_bytes())]);
+    let mut spi = SpiMock::new(&[read_txn(0x07, &0x0001_u16.to_be_bytes())].concat());
     let mut ina = INA239::new(spi.clone());
     ina.current().unwrap();
     spi.done();
@@ -127,7 +124,7 @@ fn test_current() {
 // Power result test
 #[test]
 fn test_power() {
-    let mut spi = SpiMock::new(&[read_txn(0x08, &u24_be_bytes(0x0020))]);
+    let mut spi = SpiMock::new(&[read_txn(0x08, &u24_be_bytes(0x0020))].concat());
     let mut ina = INA239::new(spi.clone());
     ina.power().unwrap();
     spi.done();
@@ -138,7 +135,7 @@ fn test_power() {
 fn test_shunt_overvolt() {
     // 0x7FFF (32727) * 1.25uv = 0.04 V
     let exp_val = exp_shunt_volt(0.04, true) as u16;
-    let mut spi = SpiMock::new(&[write_txn(0x0C, exp_val)]);
+    let mut spi = SpiMock::new(&[write_txn(0x0C, exp_val)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_shunt_overvoltage_th(0.04).unwrap();
     spi.done();
@@ -147,13 +144,18 @@ fn test_shunt_overvolt() {
 // Tests get shunt overvoltage threshold
 #[test]
 fn test_get_shunt_overvoltage() {
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0x0C, &0x7FFF_u16.to_be_bytes()),
         read_txn(0x0C, &0x8001_u16.to_be_bytes()),
-    ]);
+    ]
+    .concat();
+
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
+
     let p_val = ina.get_shunt_overvoltage_th().unwrap();
     let n_val = ina.get_shunt_overvoltage_th().unwrap();
+
     debug_assert!(p_val >= 0.0, "Expected positive value");
     debug_assert!(n_val < 0.0, "Expected negative value");
     spi.done();
@@ -164,7 +166,7 @@ fn test_get_shunt_overvoltage() {
 fn test_shunt_undervolt() {
     // 0x8000 (-32728) * 1.25uv = -0.04 V
     let exp_val = exp_shunt_volt(-0.04, true) as u16;
-    let mut spi = SpiMock::new(&[write_txn(0x0D, exp_val)]);
+    let mut spi = SpiMock::new(&[write_txn(0x0D, exp_val)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_shunt_undervoltage_th(-0.04).unwrap();
     spi.done();
@@ -173,13 +175,18 @@ fn test_shunt_undervolt() {
 // Tests get shunt undervoltage threshold
 #[test]
 fn test_get_shunt_undervoltage() {
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0x0D, &0x7FFE_u16.to_be_bytes()),
         read_txn(0x0D, &0x8000_u16.to_be_bytes()),
-    ]);
+    ]
+    .concat();
+
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
+
     let p_val = ina.get_shunt_undervoltage_th().unwrap();
     let n_val = ina.get_shunt_undervoltage_th().unwrap();
+
     debug_assert!(p_val >= 0.0, "Expected positive value, got {}", p_val);
     debug_assert!(n_val < 0.0, "Expected negative value, got {}", n_val);
     spi.done();
@@ -191,7 +198,7 @@ fn test_get_shunt_undervoltage() {
 fn test_bus_overvolt() {
     // 0x7FFF (32727) * 3.125mv = 102.2 V
     let exp_val = (102.0 / 3.125e-3) as u16;
-    let mut spi = SpiMock::new(&[write_txn(0x0E, exp_val)]);
+    let mut spi = SpiMock::new(&[write_txn(0x0E, exp_val)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_bus_overvoltage_th(102.0).unwrap();
     spi.done();
@@ -202,11 +209,13 @@ fn test_bus_overvolt() {
 fn test_get_bus_overvoltage() {
     // 0x7FFF (Reserved bit = 0)
     // 0xFFFF (Reserved bit = 1)
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0x0E, &0x7FFF_u16.to_be_bytes()),
         read_txn(0x0E, &0xFFFF_u16.to_be_bytes()),
-    ]);
+    ]
+    .concat();
 
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
 
     let val_res_zero = ina.get_bus_overvoltage_th().unwrap();
@@ -225,7 +234,7 @@ fn test_get_bus_overvoltage() {
 fn test_bus_undervolt() {
     // 0x1 * 3.125mv = 0.003 V
     let exp_val = (0.003 / 3.125e-3) as u16;
-    let mut spi = SpiMock::new(&[write_txn(0x0F, exp_val)]);
+    let mut spi = SpiMock::new(&[write_txn(0x0F, exp_val)].concat());
     let mut ina = INA239::new(spi.clone());
     ina.set_bus_undervoltage_th(0.003).unwrap();
     spi.done();
@@ -236,11 +245,13 @@ fn test_bus_undervolt() {
 fn test_get_bus_undervoltage() {
     // 0x0000 (Reserved bit = 0)
     // 0x8000 (Reserved bit = 1)
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0x0F, &0x0000_u16.to_be_bytes()),
         read_txn(0x0F, &0x8000_u16.to_be_bytes()),
-    ]);
+    ]
+    .concat();
 
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
 
     let val_res_zero = ina.get_bus_undervoltage_th().unwrap();
@@ -263,9 +274,11 @@ fn test_temp_overlimit() {
     // 0x7FF (2047) * 125mC = 255.8
     let pos_val = exp_temp_th(255.8) as u16;
 
-    let expectations = [write_txn(0x10, neg_val), write_txn(0x10, pos_val)];
-    let mut spi = SpiMock::new(&expectations);
+    let expected_txns = [write_txn(0x10, neg_val), write_txn(0x10, pos_val)].concat();
+
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
+
     ina.set_temperature_limit(-256.1).unwrap();
     ina.set_temperature_limit(255.8).unwrap();
     spi.done();
@@ -274,10 +287,13 @@ fn test_temp_overlimit() {
 // Get method
 #[test]
 fn test_get_temp_overlimit() {
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0x10, &0x7FF0_u16.to_be_bytes()),
         read_txn(0x10, &0x8000_u16.to_be_bytes()),
-    ]);
+    ]
+    .concat();
+
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
 
     let p_val = ina.get_temperature_limit().unwrap();
@@ -299,11 +315,13 @@ fn test_get_temp_overlimit() {
 fn test_get_temp_overlimit_reserved_zero() {
     // 0x7FF0 (Reserved bit[3:0] = 0)
     // 0x7FFF (Reserved bit[3:0] = 1)
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0x10, &0x7FF0_u16.to_be_bytes()),
         read_txn(0x10, &0x7FFF_u16.to_be_bytes()),
-    ]);
+    ]
+    .concat();
 
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
 
     let val_res_zero = ina.get_temperature_limit().unwrap();
@@ -326,8 +344,11 @@ fn test_power_overlimit() {
     // 0xFFFF (65535) * 256 * 0.2 * curr_lsb = 1023.
     let exp_val = (1000.0 / (256.0 * power_lsb)) as u16;
 
-    let mut spi = SpiMock::new(&[write_txn(0x02, shunt_cal), write_txn(0x11, exp_val)]);
+    let expected_txns = [write_txn(0x02, shunt_cal), write_txn(0x11, exp_val)].concat();
+
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
+
     ina.set_shunt_calibrate(10.0, 0.01).unwrap();
     ina.set_power_limit(1000.0).unwrap();
     spi.done();
@@ -336,7 +357,7 @@ fn test_power_overlimit() {
 // Get power over-limit
 #[test]
 fn test_get_power_overlimit() {
-    let mut spi = SpiMock::new(&[read_txn(0x11, &0xFFFF_u16.to_be_bytes())]);
+    let mut spi = SpiMock::new(&[read_txn(0x11, &0xFFFF_u16.to_be_bytes())].concat());
     let mut ina = INA239::new(spi.clone());
     let val = ina.get_power_limit().unwrap();
     debug_assert!(val >= 0.0, "Expected positive value");
@@ -348,7 +369,7 @@ fn test_get_power_overlimit() {
 // Test Diagnostic Flags read
 #[test]
 fn test_diag_flags() {
-    let mut spi = SpiMock::new(&[read_txn(0xB, &0xFFFF_u16.to_be_bytes())]);
+    let mut spi = SpiMock::new(&[read_txn(0xB, &0xFFFF_u16.to_be_bytes())].concat());
     let mut ina = INA239::new(spi.clone());
     let val = ina.diagnostic_flags().unwrap();
     // Check reserved bits b[11-10, 8] are zero
@@ -359,10 +380,12 @@ fn test_diag_flags() {
 // Test alert pin configuration
 #[test]
 fn test_alert_config() {
-    let mut spi = SpiMock::new(&[
+    let expected_txns = [
         read_txn(0xB, &0x0FFF_u16.to_be_bytes()),
         write_txn(0xB, 0xE2FF),
-    ]);
+    ]
+    .concat();
+    let mut spi = SpiMock::new(&expected_txns);
     let mut ina = INA239::new(spi.clone());
     ina.config_alerts(AlertConfig {
         alert_latch: true,
@@ -375,22 +398,30 @@ fn test_alert_config() {
 }
 
 // ---- Helper functions ----
-fn read_txn(reg: u8, bytes: &[u8]) -> SpiTransaction<u8> {
+fn read_txn(reg: u8, bytes: &[u8]) -> [SpiTransaction<u8>; 3] {
     // Command byte for INA239 Read: Bit 7 = 1 (Read), Address B6-B1: Register address shifted left 1
     let cmd = 0x80 | ((reg & 0x3F) << 1);
     let expt_send = vec![cmd, 0x00, 0x00];
     let mock_resp = vec![0x00, bytes[0], bytes[1]];
 
-    SpiTransaction::transfer_in_place(expt_send, mock_resp)
+    [
+        SpiTransaction::transaction_start(),
+        SpiTransaction::transfer_in_place(expt_send, mock_resp),
+        SpiTransaction::transaction_end(),
+    ]
 }
 
-fn write_txn(reg: u8, value: u16) -> SpiTransaction<u8> {
+fn write_txn(reg: u8, value: u16) -> [SpiTransaction<u8>; 3] {
     // Command byte for INA239 Write: Bit 7 = 0 (write), Address B6-B1: Register address shifted left 1
     let cmd = (reg & 0x3F) << 1;
     let bytes = value.to_be_bytes();
     let expt_send = vec![cmd, bytes[0], bytes[1]];
 
-    SpiTransaction::write_vec(expt_send)
+    [
+        SpiTransaction::transaction_start(),
+        SpiTransaction::write_vec(expt_send),
+        SpiTransaction::transaction_end(),
+    ]
 }
 
 fn u24_be_bytes(value: u32) -> [u8; 3] {
